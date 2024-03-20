4f62993d6681bb0f8fc67453776b1a85db6d0103
zhang.zuogong <zhang.zuogong@chinamworld.com>
2024-03-20 15:31:26 +0800

    代码

--- py/meifu/import.py
+++ py/meifu/import.py
@@ -1,11 +1,15 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+
+
+print(f"开始")
 # 读取 CSV 文件
-data = pd.read_csv('/Users/zhangzuogong/Desktop/Sheet1.csv')
+data = pd.read_excel('/Users/zhangzuogong/Desktop/setmmm.xlsx')
 
 # 连接到 MySQL 数据库
 engine = create_engine('mysql+mysqlconnector://weizhengbo:Password.1@139.224.197.121:63306/ods_海量2023')
 
 # 将数据写入数据库（自动创建表）
-data.to_sql('tmp_shuijun_and_qingan', con=engine, if_exists='replace', index=False, chunksize=1000)
+data.to_sql('tmp_shuijun_and_qingan', con=engine, if_exists='replace', index=False, chunksize=5000)
+print(f"结束")
---
+++ py/meifu/merge5.py
@@ -0,0 +1,92 @@
+import pymysql
+import time
+
+# 连接到数据库
+conn = pymysql.connect(
+    host='139.224.197.121',
+    port=63306,
+    user='weizhengbo',
+    password='Password.1',
+    database='ods_海量2023'
+)
+cursor = conn.cursor()
+
+
+start_time = time.time()  # 记录开始时间
+
+cursor.execute("DROP TABLE IF EXISTS meifu_analysis_platform_main_2023_4 ;")
+print(f"删除表:meifu_analysis_platform_main_2023_4")
+# 创建表格
+cursor.execute('''      create table meifu_analysis_platform_main_2023_4 as 
+    select 
+          `quarter`,
+            title,
+            is_meifu_maintain,
+            is_brand_kol,
+            is_comment,
+            webpageUrl,
+            webpageUrl_new,
+            captureWebsiteNew,
+            publishedMinute,
+            originType,
+            author,
+            summary,
+            summary_new,
+            province,
+            city,
+            originTypeThird,
+            secondTradeList,
+            originAuthorId,
+            referenceKeywordNew,
+            shareCount,
+            comments,
+            praiseNum,
+            forwardNumber,
+            favouritesCount,
+            fansNumber,
+            friendsCount,
+            gender,
+            icpProvince,
+            id,
+            publishedDay,
+            readCount,
+            subDomain,
+            titleHs,
+            topicInteractionCount,
+            weiboTopicKeyword,
+            weiboUserBeanNew,
+            zaikanCount,
+            description,
+            content,
+            functions,
+            formulations ,
+            cost_performance ,
+            product_attributes,
+            referenceKeyword,
+            category,
+            brand,
+            scene,
+            commentContent,
+              case when b.id is not null then b.sentiment_flag else a.sentiment_flag end  sentiment_flag,
+             case when b.id is not null then b.shuijun else a.shuijun end shuijun ,
+            project_name
+         from meifu_analysis_platform_main_2023_10 a 
+         left join tmp_shuijun_and_qingan b 
+              on a.id = b.id 
+       
+ 
+               
+               ''')
+
+
+
+
+
+
+end_time = time.time()  # 记录结束时间
+execution_time = end_time - start_time
+print(f"创建表格执行时间：{execution_time}秒")
+
+# 提交更改并关闭连接
+conn.commit()
+conn.close()
