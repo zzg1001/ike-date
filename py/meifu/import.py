22c878954ecc4eb27eec0c42aecc78d52e666b5e
zzg1001 <zuogong.zhang@ike-data.com>
2024-04-09 11:15:42 +0800

    godiva

--- py/meifu/import.py
+++ py/meifu/import.py
@@ -1,15 +1,25 @@
-import pandas as pd
-from sqlalchemy import create_engine
+import pymysql
+import time
+
+# 连接到数据库
+conn = pymysql.connect(
+    host='139.224.197.121',
+    port=63306,
+    user='weizhengbo',
+    password='Password.1',
+    database='dws_meifu_all'
+)
+cursor = conn.cursor()
 
 
+start_time = time.time()  # 记录开始时间
+print("开始读取数据")
 
-print(f"开始")
-# 读取 CSV 文件
-data = pd.read_excel('/Users/zhangzuogong/Desktop/setmmm.xlsx')
+cursor.execute(str(ss))
+end_time = time.time()  # 记录结束时间
+execution_time = end_time - start_time
+print(f"插入数据执行时间：{execution_time}秒")
 
-# 连接到 MySQL 数据库
-engine = create_engine('mysql+mysqlconnector://weizhengbo:Password.1@139.224.197.121:63306/ods_海量2023')
-
-# 将数据写入数据库（自动创建表）
-data.to_sql('tmp_shuijun_and_qingan', con=engine, if_exists='replace', index=False, chunksize=5000)
-print(f"结束")
+# 提交更改并关闭连接
+conn.commit()
+conn.close()
