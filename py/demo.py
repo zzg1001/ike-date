e0e76f26449ad8fe0b333f2e6d932d57ff2c6e75
zzg1001 <zuogong.zhang@ike-data.com>
2024-04-17 16:43:30 +0800

    1234

--- py/demo.py
+++ py/demo.py
@@ -1,16 +1,26 @@
-import pyodbc
-import time
-import datetime
-from datetime import date
-from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta
 
-# 连接到SQL Server数据库
-conn = pyodbc.connect('DRIVER={SQL Server};SERVER=172.16.31.42,1433;DATABASE=ODS_SRM;UID=DW_YK;PWD=DW_YK')
-# 创建游标对象
-cursor = conn.cursor()
+def get_date_range(start_date, end_date):
+    """
+    根据给定的起始日期和结束日期,生成该时间段内的每一天日期。
+    
+    参数:
+    start_date (str) - 起始日期, 格式为 "YYYY-MM-DD"
+    end_date (str) - 结束日期, 格式为 "YYYY-MM-DD"
+    
+    返回:
+    list - 包含时间段内每一天日期的列表,格式为 "YYYY-MM-DD"
+    """
+    start = datetime.strptime(start_date, "%Y-%m-%d")
+    end = datetime.strptime(end_date, "%Y-%m-%d")
+    
+    date_range = [start + timedelta(days=x) for x in range((end - start).days + 1)]
+    return [date.strftime("%Y-%m-%d") for date in date_range]
 
+# 示例使用
+start_date = "2024-01-01"
+end_date = "2024-01-03"
+dates = get_date_range(start_date, end_date)
 
-
-cnt = 1
-cnt +=1
-print(cnt )
+for date in dates:
+    print(date)
--- py/yanji/外协看板-日.py
+++ py/yanji/外协看板-日.py
@@ -1,9 +1,8 @@
 import pyodbc
 import time
-import datetime
-from datetime import date
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
-
+C
 # 连接到SQL Server数据库
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=172.16.31.42,1433;DATABASE=ODS_SRM;UID=DW_YK;PWD=DW_YK')
 # 创建游标对象
@@ -13,19 +12,17 @@ cursor = conn.cursor()
 
 def exc1(date_sql):
     
-    curr =  str(date_sql[:7])
-    # date1 = date.fromisoformat(date_sql)
-    # result = date1 - relativedelta(months=12) 
+    curr =  str(date_sql)
     laft = int(date_sql[:4])-1
-    result = str(laft)+"-01"
+    result = str(laft)+"-01-01"
     print(f"0执行{result}====={curr}")
     sql_query = f'''
                 select 
                 *
-                into Outsourcing_Dashboard.dbo.sap_base_field_histry_wide
+                into Outsourcing_Dashboard.dbo.sap_base_field_histry_date
                 from (
                     SELECT
-                    '{curr}' year_month 
+                    '{curr}' year_date 
                     ,(SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2) + '-' + SUBSTRING(EINDT, 7, 2)) eindt_date
                      ,0 is_curr_year
                      ,0 is_contain
@@ -39,7 +36,7 @@ def exc1(date_sql):
             union all
 
             SELECT 
-                    '{curr}' year_month
+                    '{curr}' year_date
                     ,(SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2) + '-' + SUBSTRING(EINDT, 7, 2)) eindt_date
                     ,case when SUBSTRING(EINDT, 1, 4)=CONVERT(varchar(4), GETDATE(), 120)  then 0 else 1 end is_curr_year
                     ,1 is_contain
@@ -58,19 +55,19 @@ def exc1(date_sql):
 
 def exc(date_sql):
       
-      curr =  date_sql[:7]
+      curr =  date_sql
       laft = int(date_sql[:4])-1
-      result = str(laft)+"-01"
+      result = str(laft)+"-01-01"
  
 
       print(f"0执行{curr}====={result}")
       sql_query = f'''
-            insert into Outsourcing_Dashboard.dbo.sap_base_field_histry_wide
+            insert into Outsourcing_Dashboard.dbo.sap_base_field_histry_date
                 select 
                 *
                 from (
                     SELECT
-                    '{curr}' year_month 
+                    '{curr}' year_date 
                     ,(SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2) + '-' + SUBSTRING(EINDT, 7, 2)) eindt_date
                      ,0 is_curr_year
                      ,0 is_contain
@@ -79,12 +76,12 @@ def exc(date_sql):
                 from Outsourcing_Dashboard.dbo.sap_base_field_wide a
                 where (LOEKZ <> 'L' or LOEKZ is null) 
                 and BUKRS='2000' and FRGKE='S' 
-            and (SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2) )='{curr}'
+            and (SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2) + '-' + SUBSTRING(EINDT, 7, 2))='{curr}'
 
             union all
 
             SELECT 
-                    '{curr}'  year_month
+                    '{curr}'  year_date
                     ,(SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2) + '-' + SUBSTRING(EINDT, 7, 2)) eindt_date
                      ,case when SUBSTRING(EINDT, 1, 4)=CONVERT(varchar(4), GETDATE(), 120)  then 0 else 1 end is_curr_year
                      ,1 is_contain
@@ -94,8 +91,8 @@ def exc(date_sql):
                 where (LOEKZ <> 'L' or LOEKZ is null) 
                 and BUKRS='2000' and FRGKE='S' 
                 and finish_or_not = '未完成' and overdue_or_not = '逾期'
-                and (SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2) )<'{curr}'
-                and (SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2))>='{result}'
+                and (SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2) + '-' + SUBSTRING(EINDT, 7, 2))<'{curr}'
+                and (SUBSTRING(EINDT, 1, 4) + '-' + SUBSTRING(EINDT, 5, 2) + '-' + SUBSTRING(EINDT, 7, 2))>='{result}'
                 ) a
                 '''
         # 将 YourTableName 替换为实际的表名
@@ -112,6 +109,24 @@ def get_all_months(start_date, end_date):
             current_date = current_date.replace(month=current_date.month + 1)
     return months
 
+def get_date_range(start_date, end_date):
+    """
+    根据给定的起始日期和结束日期,生成该时间段内的每一天日期。
+    
+    参数:
+    start_date (str) - 起始日期, 格式为 "YYYY-MM-DD"
+    end_date (str) - 结束日期, 格式为 "YYYY-MM-DD"
+    
+    返回:
+    list - 包含时间段内每一天日期的列表,格式为 "YYYY-MM-DD"
+    """
+    start = datetime.strptime(start_date, "%Y-%m-%d")
+    end = datetime.strptime(end_date, "%Y-%m-%d")
+    
+    date_range = [start + timedelta(days=x) for x in range((end - start).days + 1)]
+    return [date.strftime("%Y-%m-%d") for date in date_range]
+
+
 # 读取查询结果并打印
 # rows = cursor.fetchall()
 # for row in rows:
@@ -123,19 +138,18 @@ if __name__ == "__main__":
     
 
 
-    cursor.execute('''IF OBJECT_ID('Outsourcing_Dashboard.dbo.sap_base_field_histry_wide','U')  IS NOT NULL
-                drop table Outsourcing_Dashboard.dbo.sap_base_field_histry_wide;''')
-    print(f"删除表：sap_base_field_histry_wide")
+    cursor.execute('''IF OBJECT_ID('Outsourcing_Dashboard.dbo.sap_base_field_histry_date','U')  IS NOT NULL
+                drop table Outsourcing_Dashboard.dbo.sap_base_field_histry_date;''')
+    print(f"删除表：sap_base_field_histry_date")
 
     exc1('2015-09-01')
 
-    start_date = datetime.date(2015, 10, 1)
-    end_date = datetime.date(2024, 3, 1)
+    start_date = '2015-09-02'
+    end_date = '2024-04-17'
 
-    result = get_all_months(start_date, end_date)
+    result = get_date_range(start_date, end_date)
 
     for date_sql in result:
-        date_sql = date_sql+"-01"
         exc(date_sql)
 
     end_time = time.time()  # 记录结束时间
