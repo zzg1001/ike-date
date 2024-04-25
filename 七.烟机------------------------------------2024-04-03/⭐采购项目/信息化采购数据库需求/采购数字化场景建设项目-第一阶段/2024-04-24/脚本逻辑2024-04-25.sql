5b8bddc75112ffd6f488628f10ac6d7a4a1154c8
zzg1001 <zuogong.zhang@ike-data.com>
2024-04-25 11:27:59 +0800

    提交

--- 七.烟机------------------------------------2024-04-03/⭐采购项目/信息化采购数据库需求/采购数字化场景建设项目-第一阶段/2024-04-24/脚本逻辑2024-04-25.sql
+++ 七.烟机------------------------------------2024-04-03/⭐采购项目/信息化采购数据库需求/采购数字化场景建设项目-第一阶段/2024-04-24/脚本逻辑2024-04-25.sql
@@ -1,57 +1,38 @@
 select 
-      MATNR  as part_code
-      ,MAKTX as part_name 
- from ODS_HANA.dbo.MAKT
- 
- 
- select 
-      MEINS as '计划单位'
-     ,MATNR as part_code -- 物料代码
- from ODS_HANA.dbo.MARA
- 
- 
- select 
-     EKGRP as '采购组'        -- 采购组
-     ,MAABC as '采购重要度分类'
-     ,PLIFZ as '计划周期（天）'
-     ,MATNR as part_code -- 物料代码
- from ODS_HANA.dbo.MARC
- 
- 
-  select 
-      EKNAM as '采购组描述'
-      EKGRP as '采购组'
- from ODS_HANA.dbo.T024
+		part_code     '物料代码'            
+		,part_name    '物料技术名称'           
+		,model_specifications   '物料技术型号（规格）' 
+		,unit_code    '技术单位'              
+		,alien_code   '外来代码'        
+		,order_no     '订货号'           
+		,manufacturer '技术要求品牌(制造商)'          
+		,standard_no  '标准号'           
+		,supplier     '供应商'          
+		,item_classification_code  '项目分类'
+		,description  'description'
+        ,''           '样本（即时要求低的从数据中台抽）'		
+from DWD_WINDCHILL.dbo.dws_pdm_part_info a 
 
 
 
-     select 
-            a.MATNR   as part_code -- 物料code
-           ,a.MAKTX   as part_name -- 物料计划名称
-           ,''        as makt      -- 物料计划描述（长文本）
-           ,'2000'    as plant     -- plant
-           ,c.EKGRP   as '采购组'   -- 采购组
-           ,b.计划单位  as '计划单位'
-           ,d.EKNAM   as '采购组描述'
-           ,c.MAABC   as '采购重要度分类'
-           ,c.PLIFZ   as '计划周期（天）'
-      from ODS_HANA.dbo.MAKT a
- left join (select DISTINCT
-				   MEINS as '计划单位'
-				  ,MATNR as part_code -- 物料代码
-		     from ODS_HANA.dbo.MARA
-           ) b
-        on a.MATNR = b.part_code
- left join ODS_HANA.dbo.MARC c 
-        on a.MATNR = c.MATNR
-       and c.WERKS=2000
- left join ODS_HANA.dbo.T024 d 
-        on c.EKGRP = d.EKGRP
+select 
+supplier_cd      '采购供应商代码'
+,supplier_name   '采购供应商'
+,examine_price   '采购单价'
+,valid_end_date  '价格有效期'
+,form_num
+from ODS_SRM.dbo.srm_price_item
+
+select 
+actual_purchase_name '采购方式（招标、单一来源等）'
+,inquiry_cd          '招标文件编号'
+from ODS_SRM.dbo.srm_inq_supplier_confirm_head_field
 
 
 
 
-    select 
+
+select 
             a.MATNR   as part_code -- 物料code
            ,a.MAKTX   as part_name -- 物料计划名称
            ,''        as makt      -- 物料计划描述（长文本）
@@ -106,37 +87,3 @@ select
 			      AND new.rspos = a.require_line
              ) e
            on a.MATNR = e.material_code
-
-
-
-select 
-		part_code                 
-		,part_name               
-		,model_specifications    
-		,unit_code                
-		,alien_code               
-		,order_no                
-		,manufacturer             
-		,standard_no            
-		,supplier                
-		,item_classification_code 
-		,description              
-from DWD_WINDCHILL.dbo.dws_pdm_part_info a 
-
-
-
-
-
-
-select 
-supplier_cd
-,supplier_name
-,examine_price
-,valid_end_date
-,form_num
-from ODS_SRM.dbo.srm_price_item
-
-select 
-actual_purchase_name
-,inquiry_cd
-from ODS_SRM.dbo.srm_inq_supplier_confirm_head_field
