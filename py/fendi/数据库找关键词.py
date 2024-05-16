eaee0ef751a1e313f5139b9cb3330103b92b8d37
zhang.zuogong <zhang.zuogong@chinamworld.com>
2024-05-16 16:23:05 +0800

    1122

--- py/fendi/数据库找关键词.py
+++ py/fendi/数据库找关键词.py
@@ -31,7 +31,7 @@ def search_keyword_in_files(directory, keyword):
 
 # 指定目录和关键词
 directory = r'/Users/zhangzuogong/Desktop/test/原数据'
-keyword = '|4768|'
+keyword = 'BI_MANAGER_KEY'
 # 指定结果文件路径
 result_file_path = r'/Users/zhangzuogong/Desktop/test/rs.txt'
 
