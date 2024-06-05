47f5b4fac62c1cbac42df4ed4c958351228036ff
zzg1001 <zuogong.zhang@ike-data.com>
2024-06-05 17:32:23 +0800

    1123

--- 七.烟机------------------------------------2024-04-03/烟机工业数字大脑/采购的指标/采购.sql
+++
@@ -1,4 +0,0 @@
-SELECT SUM(contract_amount) as contract_amount
-  FROM [LeanProduction_Dashboard].[dbo].[purchase_dashboard_base_contract_info]
-  where sponsor_dept_code = '59TKR0X9'
-and contract_year between '${year_begin}' and '${year_end}'
