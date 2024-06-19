31504226782264c9b5e012593e1da794bbe747cf
zhang.zuogong <zhang.zuogong@chinamworld.com>
2024-06-19 09:37:05 +0800

    1324

--- 七.烟机------------------------------------2024-04-03/⭐烟机工业数字大脑/外购逻辑/外购逻辑2.sql
+++ 七.烟机------------------------------------2024-04-03/⭐烟机工业数字大脑/外购逻辑/外购逻辑2.sql
@@ -137,9 +137,9 @@ insert into ODS_HANA.dbo.digital_brain_outbuy_step_req
 
      select
         count(1)  -- 当前数量
-       ,sum(day_cnt)/count(1) -- 本月平均周期（
-       ,count(case when a.min_udate = CONVERT(DATE, GETDATE()) and day_cnt >1 then 1 else null end ) d -- 日新增数量
-       ,count(case when a.min_udate = CONVERT(DATE, GETDATE()) and day_cnt >1 then 1 else null end ) c -- 日消耗数量
+       ,sum(day_cnt)/count(case when EBELN is not null then 1 else 0 end) -- 本月平均周期（
+       ,count(case when a.min_udate = CONVERT(DATE, GETDATE()) and day_cnt = 0 then 1 else null end ) d -- 日新增数量
+       ,count(case when a.min_udate = CONVERT(DATE, GETDATE()) and day_cnt > 0 then 1 else null end ) c -- 日消耗数量
       from(
 
 	          select a.min_udate
@@ -163,42 +163,102 @@ insert into ODS_HANA.dbo.digital_brain_outbuy_step_req
 
 		 
 --3订单执行（计划员）=================================--------------------------------------------- 
-        
-        
-drop table  ODS_HANA.dbo.digital_brain_outbuy_step_delivery;
+
+
+
+  drop table  ODS_HANA.dbo.digital_brain_outbuy_step_delivery;
+--
+--
+--TRUNCATE table ODS_HANA.dbo.digital_brain_outbuy_step_delivery;
+--
+--INSERT INTO ODS_HANA.dbo.digital_brain_outbuy_step_delivery
+select 
+-- 订单执行（计划员）
+count(action)  --当前数量
+,sum(DATEDIFF(day,CREATE_TS,max_updated))/count(action)  -- 本月平均周期
+,count(case when DATEDIFF(day,CREATE_TS,max_updated) = 0 then 1 else null end) -- 日新增数量
+,count(case when DATEDIFF(day,CREATE_TS,max_updated) > 0 then 1 else null end) -- 日消耗数量
+
+-- 订单执行（计划员）
+,sum(delivery_cnt) --当前数量
+,sum(day_cnt)/sum(num_cnt) -- 本月平均周期
+
+
+,count(delivery_t) 
+,count(quality)
+,count(md_cnt)
+from ODS_HANA.dbo.digital_brain_outbuy_step_delivery
 
 
 TRUNCATE table ODS_HANA.dbo.digital_brain_outbuy_step_delivery;
-
+--
 INSERT INTO ODS_HANA.dbo.digital_brain_outbuy_step_delivery
+
 select 
-             a.ORDER_NUM
-            ,b.ORDER_ITEM_NUM  action
-            ,c.order_item_id   delivery
-            ,d.order_item_id   break
+             b.ORDER_ITEM_NUM  action
+            ,c.delivery_cnt    delivery_cnt
+            ,d.delivery_t      delivery_t
             ,e.order_item_num  quality 
-            ,f.order_item_num  md
+            ,f.md_cnt           md_cnt
+            
+            ,CONVERT(DATE, a.CREATED_TS)  CREATE_TS    -- 订单行首次审批通过时间
+            ,CONVERT(DATE, c.max_updated) max_updated -- 订单行创建装运单时间（最晚）
+
+            
+            ,num_cnt     -- 订单配送（供应商）
+            ,day_cnt
+
+            ,insp_cnt      -- 6.质检
+            ,quantity
+            
+            ,md_max_date   -- 7.开票
+            ,md_create_date
+            
             ,GETDATE() etl_time
-          into  ODS_HANA.dbo.digital_brain_outbuy_step_delivery
+        -- into  ODS_HANA.dbo.digital_brain_outbuy_step_delivery
        from ODS_SRM.dbo.srm_poc_order_hd a
        join ODS_SRM.dbo.srm_poc_order_item b
          on a.ID = b.ORDER_ID 
+        and b.DELETE_FLAG = 0
       left join (  
-                   SELECT b.order_item_id
-                         ,b.order_item_num 
-                         ,min(a.updated_ts) 
+
+
+                   SELECT b.order_item_id 
+                         ,b.order_item_num
+                         ,b.order_num
+                         ,count(case when a.DN_STATUS = 'SENT_AUDITED' then b.order_item_id else null end) delivery_cnt
+                         ,MAX(case when a.DN_STATUS = 'SENT_AUDITED' then a.updated_ts else null end)       max_updated
+                        ,count(case when current_status = '点收节点/结束指令' then a.updated_ts else null end) num_cnt
+                        ,count(DATEDIFF(day,
+					                         case when current_status = '点收节点/结束指令' then a.created_ts else null end
+					                        ,case when current_status = '点收节点/结束指令' then b.created_ts else null end      
+                                         )
+                               ) day_cnt 
+                        ,count( case when 
+                        	       DATEDIFF(day,
+					                         case when current_status = '点收节点/结束指令' then a.created_ts else null end
+					                        ,case when current_status = '点收节点/结束指令' then b.created_ts else null end      
+                                            ) = 0 then 1 else null end
+                               ) day_cnt_curr 
+                        ,count( case when 
+                        	       DATEDIFF(day,
+					                         case when current_status = '点收节点/结束指令' then a.created_ts else null end
+					                        ,case when current_status = '点收节点/结束指令' then b.created_ts else null end      
+                                            ) > 0 then 1 else null end
+                               ) day_cnt_ne 
 				     FROM ODS_SRM.dbo.srm_poc_delivery_note_hd a
 				     join ODS_SRM.dbo.srm_poc_delivery_note_item b
 				       ON a.id = b.dn_hd_id
-				    WHERE  a.DN_STATUS = 'SENT_AUDITED'
-	                  and CONVERT(DATE, a.updated_ts) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
-	                  and CONVERT(DATE, a.updated_ts)<=CONVERT(DATE, GETDATE()) 
-	                group by b.order_item_id
-	                     ,b.order_item_num 
-	                )c
+				    WHERE  CONVERT(DATE, a.updated_ts) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
+	                  and CONVERT(DATE, a.updated_ts)<=CONVERT(DATE, GETDATE())
+	                 group by b.order_item_id 
+	                     ,b.order_item_num
+                         ,b.order_num
+	             
+	              )c
           on b.id = c.order_item_id
 	  left join ( 
-	            	  SELECT b.order_item_id 
+	            	  SELECT b.order_item_id  delivery_t
 				      FROM ODS_SRM.dbo.srm_poc_delivery_note_hd a
 				      join ODS_SRM.dbo.srm_poc_delivery_note_item b
 				        ON a.id = b.dn_hd_id
@@ -219,120 +279,86 @@ select
 			                 )
 	                group by b.order_item_id 
 		        )d
-         on b.id = d.order_item_id
-
-    left join (select distinct order_item_num 
-    	        from ODS_SRM.dbo.insp_lot 
-    	        where ud_flag=0
+         on b.id = d.delivery_t
+    left join ( 
+	            	  SELECT b.order_item_id  delivery_t
+				      FROM ODS_SRM.dbo.srm_poc_delivery_note_hd a
+				      join ODS_SRM.dbo.srm_poc_delivery_note_item b
+				        ON a.id = b.dn_hd_id
+				   WHERE  a.DN_STATUS = 'SENT_AUDITED'
+	                 and CONVERT(DATE, a.updated_ts) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
+	                 and CONVERT(DATE, a.updated_ts)<=CONVERT(DATE, GETDATE()) 
+				     AND EXISTS (
+						  SELECT 1
+						    FROM ODS_SRM.dbo.srm_poc_delivery_note_item
+						   WHERE order_num = b.order_num
+						     AND current_status = '点收节点/结束指令'
+			             )
+			           AND NOT EXISTS (
+			             SELECT 1
+			               FROM ODS_SRM.dbo.srm_poc_delivery_note_item
+			              WHERE order_num = b.order_num
+			                AND current_status = '收货节点/结束指令'
+			                 )
+	                group by b.order_item_id 
+		        )dd
+         on b.id = dd.delivery_t
+    left join (   
+                  select 
+			             order_num 
+			             ,order_item_num
+			             ,DATEDIFF(day,ud_date_time ,create_date_time)  insp_cnt
+			             ,quantity
+			       from  (
+				          select  order_num 
+				                 ,order_item_num
+			    	         from ODS_SRM.dbo.insp_lot 
+			    	        where ud_flag=0
+			    	        group by order_num 
+			    	              ,order_item_num
+		    	         )a
+		    	         
+			         union 
+			              select 
+			                 distinct 
+			                   a.order_num 
+			                  ,order_item_num
+			                  ,0 insp_cnt
+			                  ,0 quantity
+		    	          from ODS_SRM.dbo.srm_poc_order_hd a
+				          join ODS_SRM.dbo.srm_poc_order_item b
+				            on a.ID = b.ORDER_ID 
+				           and b.DELETE_FLAG = 0
+				         where a.DELETE_FLAG = 0
+				           and b.EXAMINATION_FLAG =0
+					       and CONVERT(DATE, a.CREATED_TS) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
+						   and CONVERT(DATE, a.CREATED_TS)<=CONVERT(DATE, GETDATE()) 	
     	     )e 
-           on e.order_item_num = d.ORDER_ITEM_NUM  
-    left join (  
-		    	  SELECT 
-		              distinct order_item_num
+           on e.order_item_num = c.ORDER_ITEM_NUM  
+          and e.order_num = c.ORDER_NUM  
+
+    -- 7.开票
+    left join (                        
+		    	
+	           SELECT order_item_id
+		                ,count(CASE when  b.INVOICE_QTY<> b.QTY then order_item_id else null end) md_ne_cnt
+		               ,max( CASE when  b.INVOICE_QTY= b.QTY then b.created_ts else null end) as md_max_date
+		               ,max(CASE when  b.INVOICE_QTY=b.QTY then a.created_ts else null end) as md_create_date
+		               ,count(CASE when  b.INVOICE_QTY=b.QTY then a.created_ts else null end) md_eq_cnt
 		          from ODS_SRM.dbo.srm_poc_md_hd a
 		     left join ODS_SRM.dbo.srm_poc_md_item b 
 		            on a.id = b.MD_HD_ID 
-		            where b.INVOICE_QTY<> b.QTY 
-		             and CONVERT(DATE, a.created_ts) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
-		             and CONVERT(DATE, a.created_ts)<=CONVERT(DATE, GETDATE()) 
+		         where  CONVERT(DATE, a.created_ts) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
+		           and CONVERT(DATE, a.created_ts)<=CONVERT(DATE, GETDATE()) 
+		           group by order_item_id
                )f 
-           on f.order_item_num = e.order_item_num 
+           on f.order_item_id = b.id 
        where a.DELETE_FLAG = 0
          and CONVERT(DATE, a.CREATED_TS) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
 	     and CONVERT(DATE, a.CREATED_TS)<=CONVERT(DATE, GETDATE()) 	 
-         
-
-
-select 
-COUNT(1) 
-,count(order_item_id)
-,count(order_item_n)
-,count(quality)
-,count(md)
-from ODS_HANA.dbo.digital_brain_outbuy_step_delivery a
-
-    
-
---4订单配送（供应商）---------------==============---------------------------------- 	
-            SELECT b.order_num
-                  ,b.order_item_num
-                  ,CONVERT(DATE, a.CREATED_TS) CREATED_TS
-              from ODS_SRM.dbo.srm_poc_delivery_note_hd a
-	          join ODS_SRM.dbo.srm_poc_delivery_note_item b 
-		        on a.id = b.dn_hd_id 
-		     where a.DELETE_FLAG=0 
-		       and a.DN_STATUS='SENT_AUDITED'
-			   and CONVERT(DATE, a.CREATED_TS) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
-	           and CONVERT(DATE, a.CREATED_TS)<=CONVERT(DATE, GETDATE()) 	
-		      -- and b.current_status is null 
-
-
---5物流周转（仓库）----------------------------------------------- 	
-
-	    
-SELECT a.updated_ts 
-      ,b.ORDER_NUM
-      ,b.ORDER_ITEM_NUM  
- FROM ODS_SRM.dbo.srm_poc_delivery_note_hd a
- JOIN (select dn_hd_id
-              ,ORDER_NUM
-              ,ORDER_ITEM_NUM  
-         from ODS_SRM.dbo.srm_poc_delivery_note_item
-     group by dn_hd_id
-              ,ORDER_NUM
-              ,ORDER_ITEM_NUM ) b
-   ON a.id = b.dn_hd_id
-WHERE a.DELETE_FLAG = 0
-  AND a.DN_STATUS = 'SENT_AUDITED'
-  and CONVERT(DATE, a.updated_ts) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
-  and CONVERT(DATE, a.updated_ts)<=CONVERT(DATE, GETDATE()) 	
-  AND EXISTS (
-			  SELECT 1
-			    FROM ODS_SRM.dbo.srm_poc_delivery_note_item
-			   WHERE order_num = b.order_num
-			     AND current_status = '点收节点/结束指令'
-             )
-  AND NOT EXISTS (
-             SELECT 1
-               FROM ODS_SRM.dbo.srm_poc_delivery_note_item
-              WHERE order_num = b.order_num
-                AND current_status = '收货节点/结束指令'
-                 )
-
-
-
-
-
-      select   a.order_item_id
-               ,count(1)                                                                 as flag0_cnt
-               ,count(case when current_status = '点收节点/结束指令' then 1
-                    when current_status = '收货节点/结束指令' then 0 else 1 end )           as flag1_cnt
-               ,count(case when current_status = '点收节点/结束指令' then 1 else null end ) as flag2_cnt
-          from ODS_SRM.dbo.srm_poc_delivery_note_item a
-          join ODS_SRM.dbo.srm_poc_delivery_note_hd b
-            on b.id = a.dn_hd_id  
-         where b.DELETE_FLAG = 0
-           AND b.DN_STATUS = 'SENT_AUDITED'
-           and CONVERT(DATE, a.updated_ts) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
-           and CONVERT(DATE, a.updated_ts)<=CONVERT(DATE, GETDATE()) 
-         group by order_item_id,b.updated_ts
-         order by order_item_id,b.updated_ts
-
-    
---6质检----------------------------------------------------------- 
-
-select count(1) from  ODS_SRM.dbo.insp_lot where ud_flag=0
-
---7.开票----------------------------------------------------------- 
-
-       SELECT  order_num
-              ,order_item_num
-          from ODS_SRM.dbo.srm_poc_md_hd a
-     left join ODS_SRM.dbo.srm_poc_md_item b 
-            on a.id = b.MD_HD_ID 
-            where b.INVOICE_QTY<> b.QTY 
-             and CONVERT(DATE, a.created_ts) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
-             and CONVERT(DATE, a.created_ts)<=CONVERT(DATE, GETDATE()) 
-
-
-             
+	     
+	---------------     
+	      select * from ODS_SRM.dbo.srm_poc_md_invoice_hd
+           where CONVERT(DATE, a.CREATED_TS) >=CAST(YEAR(GETDATE()) AS VARCHAR(4)) + '-01-01'  
+	        and CONVERT(DATE, a.CREATED_TS)<=CONVERT(DATE, GETDATE()) 
+	     
