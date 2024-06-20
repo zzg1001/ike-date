ef545d55f5cee75fc2fb8d694f48e594cd028a4f
zhang.zuogong <zhang.zuogong@chinamworld.com>
2024-06-20 09:20:57 +0800

    1213

--- 七.烟机------------------------------------2024-04-03/⭐采购项目/信息化采购数据库需求/采购数字化场景建设项目-第一阶段/2024-04-24/脚本逻辑2024-05-09.sql
+++ 七.烟机------------------------------------2024-04-03/⭐采购项目/信息化采购数据库需求/采购数字化场景建设项目-第一阶段/2024-04-24/脚本逻辑2024-05-09.sql
@@ -1,3 +1,6 @@
+      
+TRUNCATE table Purchase_Dashboard.dbo.base_outsourced_risk_price ;
+INSERT INTO Purchase_Dashboard.dbo.base_outsourced_risk_price 
 
 select 
 a.part_code
@@ -13,6 +16,7 @@ a.part_code
 ,stock_quantity
 ,sap_unit
 ,sap_price
+,b.windchill_part_name
 ,model_specifications
 ,source
 ,unit_code
@@ -49,8 +53,8 @@ from (
 							,material_desc  as material_desc
 							,plant_code     as plant_code
 							,stock_quantity as stock_quantity
-				           ,b.sap_unit as sap_unit                 --'计划单位'
-				           ,''         as sap_price
+				           ,b.sap_unit      as sap_unit                 --'计划单位'
+				           ,bb.sap_price    as sap_price
 				      from ODS_HANA.dbo.MAKT a
 				 left join (select DISTINCT
 								   MEINS as sap_unit
@@ -58,6 +62,13 @@ from (
 						     from ODS_HANA.dbo.MARA
 				           ) b
 				        on a.MATNR = b.part_code
+				  left join (
+				      select 
+						    CASE  WHEN MATNR LIKE '%.%' THEN REPLACE(MATNR, '\.[0-9]+$', '' ) ELSE MATNR end part_code
+						    ,lplpr  sap_price
+						    from ODS_HANA.dbo.MBEW
+				         ) bb 
+				        on a.MATNR = bb.part_code
 				 left join ODS_HANA.dbo.MARC c 
 				        on a.MATNR = c.MATNR
 				       and c.WERKS=2000
@@ -75,14 +86,37 @@ from (
 				           on a.MATNR = e.material_code
 						    where c.EKGRP in ('201','211','214','215','204','205')
            )a 
+
+
 left join (
+
+	           select   CASE
+						    WHEN part_code LIKE '%sh' OR part_code LIKE '%SH' 
+						    THEN SUBSTRING(part_code, 1, LEN(part_code) - 2)
+						    ELSE part_code
+						  END AS part_code
+						,windchill_part_name   -- '物料技术名称'           
+						,model_specifications  --'物料技术型号（规格）' 
+						,unit_code             --'技术单位'              
+						,alien_code            --'外来代码'        
+						,order_no              --'订货号'           
+						,manufacturer          --'技术要求品牌(制造商)'          
+						,standard_no           --'标准号'           
+						,supplier              -- '供应商' 
+						,source                -- '来源'   
+						,status_indication     -- '状态标识'      
+						,item_classification_code  -- '项目分类'
+						,description           -- 'description'
+				             ,'' Windchill_sample  --  '样本（即时要求低的从数据中台抽）'
+
+	               from (
 				select 
-						REPLACE(CASE 
+						CASE 
 								        WHEN PATINDEX('%[^0]%', part_code  ) > 0 
 								        THEN SUBSTRING(part_code  , PATINDEX('%[^0]%', part_code  ), LEN(part_code ))
 								        ELSE  part_code
-						  END ,'sh', '') AS part_code           
-						,part_name             -- '物料技术名称'           
+						  END AS part_code
+						,part_name windchill_part_name  -- '物料技术名称'           
 						,model_specifications  --'物料技术型号（规格）' 
 						,unit_code             --'技术单位'              
 						,alien_code            --'外来代码'        
@@ -95,8 +129,8 @@ left join (
 						,item_classification_code  -- '项目分类'
 						,description           -- 'description'
 				        ,'' Windchill_sample  --  '样本（即时要求低的从数据中台抽）'		
-				from DWD_WINDCHILL.dbo.dws_pdm_part_info a 
-			      where is_new=1
+				from DWD_WINDCHILL.dbo.dws_pdm_part_info 
+				)a
           ) b
        on a.part_code = b.part_code
 left join  [ODS_HANA].[dbo].[LTXT_purchase] m 
@@ -145,4 +179,3 @@ left join (
 			      and b.DELETED = 0
       ) c
       on  a.part_code = c.part_code
-      
