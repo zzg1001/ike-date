545f087abcdf74d3a5712ae3934eb35b4df4ad0f
zzg1001 <zuogong.zhang@ike-data.com>
2025-02-20 14:29:49 +0800

    新工时

--- 烟机/redmine 工时.sql
+++ 烟机/redmine 工时.sql
@@ -1,3 +1,4 @@
+
 with tmp as (
 select 
 distinct
@@ -13,21 +14,34 @@ d.name as tracker_name,
 f.name as issue_status,
 g.comments,
 g.hours,
+j.value is_over_hour, 
 b.created_on,
 a.start_date,
 a.due_date,
 a.done_ratio
 from ODS_REDMINE.dbo.issues a
-left join ODS_REDMINE.dbo.projects b on a.project_id=b.id
-left join ODS_REDMINE.dbo.trackers d on a.tracker_id=d.id
-left join ODS_REDMINE.dbo.users e on a.assigned_to_id=e.id
-left join ODS_REDMINE.dbo.issue_statuses f on a.status_id=f.id
-left join ODS_REDMINE.dbo.time_entries g on a.id=g.issue_id and a.project_id=g.project_id
-left join ODS_REDMINE.dbo.users c on g.user_id=c.id
-left join ODS_REDMINE.dbo.enumerations h on g.activity_id=h.id
-left join ODS_REDMINE.dbo.users i on a.author_id=i.auth_source_id
-where g.spent_on>='2024-01-01' and g.spent_on<='2024-11-18'
+left join ODS_REDMINE.dbo.projects b 
+       on a.project_id=b.id
+left join ODS_REDMINE.dbo.trackers d 
+       on a.tracker_id=d.id
+left join ODS_REDMINE.dbo.users e 
+       on a.assigned_to_id=e.id
+left join ODS_REDMINE.dbo.issue_statuses f 
+       on a.status_id=f.id
+left join ODS_REDMINE.dbo.time_entries g 
+       on a.id=g.issue_id 
+			and a.project_id=g.project_id
+left join ODS_REDMINE.dbo.users c 
+       on g.user_id=c.id
+left join ODS_REDMINE.dbo.enumerations h 
+       on g.activity_id=h.id
+left join ODS_REDMINE.dbo.users i 
+       on a.author_id=i.auth_source_id
+left join  ODS_REDMINE.dbo.custom_values j 
+       on g.id = j.customized_id
+		  and custom_field_id =9
+where g.spent_on>='2025-01-01' and g.spent_on<='2025-01-31'
 )
 select * from tmp 
-where  user_name like '%yk%' 
+where  user_name like '%yk%'
 order by spent_on
